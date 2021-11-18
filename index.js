ec5ed9f997c2ee21d4757ce98af52ff05cb03cfb
유민호 <wingedcompany@gmail.com>
2021-11-19 01:06:22 +0900

    start 2
    1. 3개를 연속
    2. 두개 하고 뒤집어지기
    3. 두개 맞으면 뒤집어져있고, 또 4개할때 뒤집어져있다.?

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const x = 4;
-const y = 3;
+const x = 3;
+const y = 4;
 //카드 뒤 색깔 7개 2개씩 미리 설정
-const backColor = ['#FFC312', '#FFC312', '#C4E538', '#C4E538', '#12CBC4', '#12CBC4', '#ED4C67', '#ED4C67', '#FDA7DF', '#FDA7DF', '#833471', '#833471']
+const backColor = [
+    '#FFC312', '#FFC312',
+    '#C4E538', '#C4E538',
+    '#12CBC4', '#12CBC4',
+    '#ED4C67', '#ED4C67',
+    '#FDA7DF', '#FDA7DF',
+    '#833471', '#833471',
+    '#FFFFFF', '#FFFFFF',
+    '#999999', '#999999'
+]
 
 //위의 색깔 랜덤 섞기 - 피셔 예이츠 방식
 let chosenColor = [];
@@ -13,26 +22,67 @@ while (backColor.length > 0) {
 }
 console.log(chosenColor);
 
+var aa;
+
 function cardSetting(x, y) {
     //카드 세팅
     for (let i = 0; i < x * y; i++) {
         const card = document.createElement('div');
-        card.className = 'card';
+        card.className = 'card wait';
         const cardInner = document.createElement('div');
         cardInner.className = 'card-inner';
         const cardFront = document.createElement('div');
         cardFront.className = 'card-front';
         const cardBack = document.createElement('div');
         cardBack.className = 'card-back';
+        cardBack.dataset.color = chosenColor[i];
         cardBack.style.backgroundColor = chosenColor[i];
         cardInner.appendChild(cardFront);
         cardInner.appendChild(cardBack);
         card.appendChild(cardInner);
-        document.body.appendChild(card);
+        document.querySelector(".center-space").appendChild(card);
 
         //클릭하여 카드 뒤집기
-        card.addEventListener('click', function() {
+        card.addEventListener('click', function () {
+
+            if (card.classList.contains("completed")) return;
+
             card.classList.toggle('flipped');
+            card.classList.toggle('on');
+
+            const onEl = document.querySelectorAll(".on");
+
+            if (onEl.length === 0) {
+                return;
+            }
+
+            if (onEl.length > 1) {
+                return;
+            }
+
+            if (card.classList.contains("flipped")) {
+                card.classList.add('on');
+            } else {
+                card.classList.remove('on');
+            }
+
+            const oneColor = onEl[0].querySelector(".card-back").dataset.color
+            const twoColor = card.querySelector(".card-back").dataset.color
+
+            if (oneColor === twoColor) {
+                onEl[0].classList.add('completed');
+                card.classList.add('completed');
+                return;
+            }
+
+            setTimeout(() => {
+                if (oneColor !== twoColor) {
+                    onEl[0].classList.toggle('flipped');
+                    card.classList.toggle('flipped');
+                }
+                onEl[0].classList.remove('on');
+                card.classList.remove('on');
+            }, 1000)
         });
     }
 }
