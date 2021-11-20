5e48146fa9462632b6de9f7cd3bdc71044a5955a
유민호 <wingedcompany@gmail.com>
2021-11-21 00:48:33 +0900

    start 4
    디자인만 하면 될듯?
    타이머가 있어야하나?

--- index.js
+++ index.js
@@ -1,100 +1,112 @@
-const x = 3;
-const y = 4;
-//카드 뒤 색깔 7개 2개씩 미리 설정
-const backColor = [
-    '#FFC312', '#FFC312',
-    '#C4E538', '#C4E538',
-    '#12CBC4', '#12CBC4',
-    '#ED4C67', '#ED4C67',
-    '#FDA7DF', '#FDA7DF',
-    '#833471', '#833471',
-    '#FFFFFF', '#FFFFFF',
-    '#999999', '#999999'
-]
-
-//위의 색깔 랜덤 섞기 - 피셔 예이츠 방식
-let chosenColor = [];
-
-while (backColor.length > 0) {
-    let color = Math.floor(Math.random() * backColor.length); //0-13까지 정수
-    let mixedColor = backColor.splice(color, 1)[0];
-    chosenColor.push(mixedColor);
+const config = {
+    x: 3,
+    y: 4,
+    data: [
+        {color: '#FFC312', text: '채팅'},
+        {color: '#C4E538', text: '올인원'},
+        {color: '#12CBC4', text: '협업'},
+        {color: '#ED4C67', text: '프로젝트'},
+        {color: '#FDA7DF', text: '업무'},
+        {color: '#833471', text: '워크'},
+    ]
 }
-console.log(chosenColor);
 
-var aa;
+document.addEventListener("DOMContentLoaded", () => {
+    cardSetting(config.x, config.y);
+});
+
+function getRandomColorArray() {
+    //위의 색깔 랜덤 섞기 - 피셔 예이츠 방식
+    let chosenColor = [];
+    const colors = config.data.map(v => v.color);
+    const backColor = [...colors, ...colors]
+    while (backColor.length > 0) {
+        let color = Math.floor(Math.random() * backColor.length); //0-13까지 정수
+        let mixedColor = backColor.splice(color, 1)[0];
+        chosenColor.push(mixedColor);
+    }
+    return chosenColor;
+}
 
 function cardSetting(x, y) {
+
+    const randomColorArray = getRandomColorArray();
+
+    let prevCard;
+    let timer;
+
     //카드 세팅
     for (let i = 0; i < x * y; i++) {
+
         const card = document.createElement('div');
-        card.className = 'card wait';
+        card.className = 'card wait flipped';
+
         const cardInner = document.createElement('div');
         cardInner.className = 'card-inner';
+
         const cardFront = document.createElement('div');
         cardFront.className = 'card-front';
+
+        const cardImage = document.createElement('div');
+        cardImage.className = 'card-image';
+        cardFront.appendChild(cardImage);
+
+        cardInner.appendChild(cardFront);
+
         const cardBack = document.createElement('div');
         cardBack.className = 'card-back';
-        cardBack.dataset.color = chosenColor[i];
-        cardBack.style.backgroundColor = chosenColor[i];
-        cardInner.appendChild(cardFront);
+        cardBack.dataset.color = randomColorArray[i];
+        cardBack.style.backgroundColor = randomColorArray[i];
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = config.data.find(v => v.color === randomColorArray[i]).text;
+        cardBack.appendChild(cardText);
+
         cardInner.appendChild(cardBack);
+
         card.appendChild(cardInner);
         document.querySelector(".center-space").appendChild(card);
 
-        //클릭하여 카드 뒤집기
-
-        let prevColor, currentColor;
-
         card.addEventListener('click', function () {
 
+            if (timer) return;
             if (!card.classList.contains("wait")) return;
             if (card.classList.contains("flipped")) return;
 
             card.classList.add('flipped');
-            currentColor = card.querySelector(".card-back").dataset.color;
 
-            if(prevColor === currentColor){
-                document.querySelectorAll('flipped').forEach(el => el.classList.remove('wait'));
-            }
+            if (!prevCard) return (prevCard = card);
 
+            const currentColor = card.querySelector(".card-back").dataset.color;
+            const prevColor = prevCard.querySelector(".card-back").dataset.color;
 
-
-
-            const onEl = document.querySelectorAll(".on");
-
-            if (onEl.length === 2) {
-                return;
-            }
-
-
-            if (card.classList.contains("flipped")) {
-                card.classList.add('on');
-            } else {
-                card.classList.remove('on');
-            }
-
-            firstColor = onEl[0].querySelector(".card-back").dataset.color;
-
-            const oneColor = onEl[0].querySelector(".card-back").dataset.color
-            const twoColor = card.querySelector(".card-back").dataset.color
-
-            if (oneColor === twoColor) {
-                onEl[0].classList.remove('wait');
-                card.classList.remove('wait');
-                return;
-            }
-
-            setTimeout(() => {
-                if (oneColor !== twoColor) {
-                    onEl[0].classList.toggle('flipped');
-                    card.classList.toggle('flipped');
+            if (prevColor === currentColor) {
+                [prevCard, card].forEach(el => el.classList.remove('wait'));
+                prevCard = null;
+                if (isDone()) {
+                    setTimeout(() => {
+                        alert('Complete!');
+                    }, 1000)
                 }
-                onEl[0].classList.remove('on');
-                card.classList.remove('on');
+                return;
+            }
+
+            timer = setTimeout(() => {
+                [prevCard, card].forEach(el => el.classList.remove('flipped'));
+                prevCard = null;
+                timer = null;
             }, 1000)
+
+            function isDone() {
+                return document.querySelectorAll('.card.wait').length === 0
+            }
         });
     }
+
+    timer = setTimeout(() => {
+        document.querySelectorAll('.card').forEach(el => el.classList.remove('flipped'))
+        timer = null;
+    }, 1000)
 }
 
-cardSetting(x, y);
